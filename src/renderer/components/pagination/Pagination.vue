04c8422eb88d0172047ed36af19b989baeed4c99
f-CJ <fcj.developer@gmail.com>
2020-01-30 16:55:26 +0100

    Undo this commit when work starts in this branch

---
+++ src/renderer/components/pagination/Pagination.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="pagination-block">      
+    <div class="select-wrapper">
+      <select style="float:left">
+        <option>50</option>
+        <option>100</option>
+      </select>
+    </div>
+    <el-pagination
+      hide-on-single-page
+      background
+      layout="prev, pager, next"
+      :total="1000">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+import Vuex from 'vuex';
+
+export default {
+  name: 'Pagination',
+
+  data() {
+    return {};
+  },
+
+  computed: {
+    ...Vuex.mapGetters([])
+  },
+
+  watch: {},
+
+  methods: {
+    ...Vuex.mapActions(['']),
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/_variables.scss';
+  .pagination-block {
+    text-align:right;
+
+    div.select-wrapper {
+      width: 65px;
+      display: inline-block;
+    }
+
+    div.el-pagination { 
+      display: inline-block;
+    }
+  }
+</style>
